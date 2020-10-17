9dab39b479ea3a9ef2135a24745ca424e6e204dc
ken <heyty005@unisa.edu.au>
2020-10-17 15:28:06 +1030

    Clean up

--- Photon/Src/Photon.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-//For use by photon application
-#include "Photon\Application.h"
-#include <stdio.h>
-#include <iostream>
-//------------Entry point-----------
-#include "Photon\EntryPoint.h"
